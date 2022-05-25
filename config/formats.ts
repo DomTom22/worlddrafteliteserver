494e55376b856f6779c71970a13a4b1cf9256afa
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-05-25 09:31:41 -0500

    update

--- config/formats.ts
+++ config/formats.ts
@@ -972,7 +972,7 @@ export const Formats: FormatList = [
 				`&bullet; <a href="https://www.smogon.com/forums/threads/3675194/">Ubers Viability Rankings</a>`,
 			],
 
-			mod: 'gen8rr',
+			mod: 'gen8',
 			ruleset: ['Standard', 'Obtainable', 'Dynamax Clause'],
 			banlist: ['AG', 'Baton Pass', 'Moody'],
 		},
@@ -984,7 +984,7 @@ export const Formats: FormatList = [
 				`&bullet; <a href="https://www.smogon.com/forums/threads/3674058/">OU Viability Rankings</a>`,
 			],
 
-			mod: 'gen8rr',
+			mod: 'gen8',
 			ruleset: ['Standard', 'Obtainable', 'Dynamax Clause'],
 			banlist: ['Uber', 'OUBL', 'AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass'],
 		},

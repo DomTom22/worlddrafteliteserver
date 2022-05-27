b131eae2be2530493ccbc46130fcd198e480b840
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-05-27 09:34:46 -0500

    test

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -240,7 +240,6 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		weightkg: 100.5,
 		color: "Black",
 		eggGroups: ["Undiscovered"],
-		battleOnly: "Charizard-Delta",
 		requiredItem: "Delta Charizardite",
 	},
 	charizardegho: {

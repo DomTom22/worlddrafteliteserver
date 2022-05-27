e2b8a123b1f4706f21569ba3460a688f1c70ee2e
Mizzy <73121203+MizzyPD@users.noreply.github.com>
2022-05-27 06:05:43 +0100

    greninja new move

--- data/mods/daybreak/moves.ts
+++ data/mods/daybreak/moves.ts
@@ -19733,3 +19733,21 @@ export const Moves: {[moveid: string]: MoveData} = {
 		contestType: "Cool",
 	},
 };
+	shadowlunge: {
+		num: 1394,
+		accuracy: 100,
+		basePower: 120,
+		category: "Physical",
+		name: "Shadow Lunge",
+		pp: 15,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, defrost: 1},
+		recoil: [33, 100],
+		secondary: {
+			chance: 10,
+			status: 'brn',
+		},
+		target: "normal",
+		type: "Dark",
+		contestType: "Cool",
+	},
